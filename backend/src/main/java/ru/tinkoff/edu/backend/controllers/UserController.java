4b4249691ebc7ca74e946e7073f1d62090e3ed71
algeps <nik17052001@outlook.com>
2022-10-12 20:12:53 +0600

    SKIPPER-9 add login without database

--- backend/src/main/java/ru/tinkoff/edu/backend/controllers/UserController.java
+++ backend/src/main/java/ru/tinkoff/edu/backend/controllers/UserController.java
@@ -19,14 +19,23 @@ public class UserController {
     @PostMapping("/registration")
     public ResponseEntity<?> registration(@RequestBody UserRegDTO newUser) {
         System.out.println(newUser);
-        final boolean correct = newUser.getFirstName() != null
+        final boolean isCorrect = newUser.getFirstName() != null
                 && newUser.getLastName() != null
                 && newUser.getPassword() != null
                 && newUser.getEmail() != null;
-        return correct ?
+        return isCorrect ?
                 new ResponseEntity<>(HttpStatus.CREATED) :
                 new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserLoginDTO loginUser) {
+        System.out.println(loginUser);
+        final boolean isExistAndCorrect = loginUser.getEmail().equals("123@email.com")
+                && loginUser.getPassword().equals("123456");
 
+        return isExistAndCorrect ?
+                new ResponseEntity<>(HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
