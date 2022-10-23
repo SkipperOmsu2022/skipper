f90f872f8954f470bf6d773102823e120bcf982c
algeps <nik17052001@outlook.com>
2022-10-23 15:07:11 +0600

    init DB

---
+++ backend/src/main/resources/database/initDB.sql
@@ -0,0 +1,12 @@
+-- Database: skipper
+
+-- DROP DATABASE IF EXISTS skipper;
+
+CREATE DATABASE skipper
+    WITH
+    OWNER = root
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
