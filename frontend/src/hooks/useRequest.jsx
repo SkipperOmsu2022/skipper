4806d17098a481fdf850af59884109810f9be637
Ilya <rogachevskii01@gmail.com>
2022-10-23 18:47:09 +0600

    SKIPPER-6 invalid request

--- frontend/src/hooks/useRequest.jsx
+++ frontend/src/hooks/useRequest.jsx
@@ -5,22 +5,23 @@ export const useRequest = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    // method = 'GET', body = null, headers = {'Content-Type': 'application/json'}
-    // {method, body, headers}
-
     const request = useCallback(async (url, data) => {
         setLoading(true);
 
-        try {
-            const response = await axios.post(url, data);
-            setLoading(false);
-
-            return response;
-        } catch (e) {
-            setLoading(false);
-            setError(e.message)
-            throw e;
-        }
+        axios.post(url, data, {headers: { "Content-Type": "application/json", "Content-Lenght": "<calculated hen request is sent>",
+                                           "User-Agent": "PostmanRuntime/7.29.2", "Accept": "*/*", "Accept-Encoding": "gzip, deflate, br",
+                                        "Connection": "keep-alive" }})
+            .then(res => {
+                setLoading(false);
+                console.log('Нормас');
+                console.log(res);
+            })
+            .catch(e => {
+                setLoading(false);
+                setError(e.message)
+                console.log('Ошибка');
+                console.log(e);
+            });
     }, []);
 
     const clearError = useCallback(() => setError(null), [])
--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -3,11 +3,10 @@ import {useRequest} from "../hooks/useRequest"
 const useService = () => {
     const {loading, setLoading, error, clearError, request} = useRequest();
 
-    const _apiBase = 'http://127.0.0.1:8080/';
+    const _apiBase = 'http://127.0.0.1:8080/api/user/registration';
 
     const testRequest = async (data) => {
-        const res = await request(`${_apiBase}`);
-        //console.log(res);
+        const res = await request(`${_apiBase}`, data);
     }
 
     return {loading, request, error, clearError, setLoading, testRequest}
