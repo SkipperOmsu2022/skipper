750f602a123e6fd048c4a9ae756422fcffec8ee8
Ilya <rogachevskii01@gmail.com>
2022-12-12 15:59:06 +0600

    changed the page structure

--- frontend/src/components/pages/MessagesPage/MessagesPage.jsx
+++ frontend/src/components/pages/MessagesPage/MessagesPage.jsx
@@ -1,72 +1,76 @@
-import cn from 'classnames';
-
-import styles from './MessagesPage.module.css';
+import './MessagesPage.scss';
 import photo from "../../../resources/profile-photo.jpg"
 
 export const MessagesPage = () => {
     return (
-    <div className={styles._}>
-        <div className={styles.section}>
-            <div className={styles.listChats}>
-                <div className={styles.listChatsItem}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
+    <div className='_'>
+        <div className='msg-section'>
+            <div className='listChats'>
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='listChatsItemFullName'>
                         Рогачевский Илья
                     </div>
                 </div>
-                <div className={cn(styles.listChatsItem, styles.listChatsItemActive)}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
+                <div className={'listChatsItem listChatsItemActive'}>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='listChatsItemFullName'>
                         Рогачевский Илья
                     </div>
                 </div>
-                <div className={styles.listChatsItem}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='{listChatsItemFullName'>
                         Рогачевский Илья
                     </div>
                 </div>
-                <div className={styles.listChatsItem}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='{listChatsItemFullName'>
                         Рогачевский Илья
                     </div>
                 </div>
-                <div className={styles.listChatsItem}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='{listChatsItemFullName'>
                         Рогачевский Илья
                     </div>
                 </div>
-                <div className={styles.listChatsItem}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='{listChatsItemFullName'>
+                        Рогачевский Илья
+                    </div>
+                </div>
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='{listChatsItemFullName'>
                         Какой-то чел с той горы
                     </div>
                 </div>
-                <div className={styles.listChatsItem}>
-                    <img className={styles.listChatsItemProfilePhoto} src={photo} alt=''/>
-                    <div className={styles.listChatsItemFullName}>
-                        Рогачевский Илья
+                <div className='listChatsItem'>
+                    <img className='listChatsItemProfilePhoto' src={photo} alt=''/>
+                    <div className='{listChatsItemFullName'>
+                    Рогачевский Илья
                     </div>
                 </div>
             </div>
-            <div className={styles.chatWindowWrapper}>
-                <div className={styles.chatWindowHeader}>
+            <div className='chatWindowWrapper'>
+                <div className='chatWindowHeader'>
                     Рогачевский Илья
                 </div>
-                <div className={styles.chatWindowBody}>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                <div className='chatWindowBody'>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Вы
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Напиши что-нибудь полезное</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <div>
                             Какое-то сообщение.Какое-то сообщениеКакое-то сообщение.
@@ -74,80 +78,80 @@ export const MessagesPage = () => {
                             Какое-то сообщениеКакое-то сообщение.Какое-то сообщение
                         </div>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
-                    <div className={styles.chatWindowBodyMessage}>
-                        <div className={styles.chatWindowBodyHeader}>
+                    <div className='chatWindowBodyMessage'>
+                        <div className='chatWindowBodyHeader'>
                             Рогачевский Илья
-                            <span className={styles.chatWindowBodyHeaderDateTime}>12:10</span>
+                            <span className='chatWindowBodyHeaderDateTime'>12:10</span>
                         </div>
                         <span>Какое-то сообщение</span>
                     </div>
                 </div>
-                <div className={styles.chatWindowInput}>
-                    <input className={styles.chatWindowInputField}/>
-                    <button className={styles.chatWindowInputButton}>{'>'}</button>
+                <div className='chatWindowInput'>
+                    <input className='chatWindowInputField'/>
+                    <button className='chatWindowInputButton'>{'>'}</button>
                 </div>
             </div>
         </div>
