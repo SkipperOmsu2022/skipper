5417bf6788eb5ecb79174b2ae3aa06cefc46aa46
Nikita Vorobev <nik17052001@outlook.com>
2023-03-16 12:25:42 +0600

    add work experience and relationships with it

---
+++ backend/src/main/java/ru/tinkoff/edu/backend/entities/WorkExperience.java
@@ -0,0 +1,29 @@
+package ru.tinkoff.edu.backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "WorkExperience")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkExperience {
+    @EmbeddedId
+    private WorkExperiencePK id = new WorkExperiencePK();
+    @Column(nullable = false, columnDefinition = "DATE")
+    private LocalDate dateStart;
+    @Column(columnDefinition = "DATE")
+    private LocalDate dateEnd;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ backend/src/main/java/ru/tinkoff/edu/backend/entities/WorkExperiencePK.java
@@ -0,0 +1,20 @@
+package ru.tinkoff.edu.backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkExperiencePK implements Serializable {
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    @Column(name = "place_of_work", nullable = false, unique = true)
+    private String placeOfWork;
+}
