3c4485e53a9db1df9a383c32baf06a25aa4cb950
Nikita Vorobev <nik17052001@outlook.com>
2023-04-05 15:37:50 +0600

    wrote controllers test layer but without MentorProfileSettingsControllerTest.java

--- backend/src/test/java/ru/tinkoff/edu/backend/controllers/AuthControllerTest.java
+++ backend/src/test/java/ru/tinkoff/edu/backend/controllers/AuthControllerTest.java
@@ -2,7 +2,6 @@ package ru.tinkoff.edu.backend.controllers;
 
 import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -24,6 +23,9 @@ import ru.tinkoff.edu.backend.dto.UserRegDTO;
 import ru.tinkoff.edu.backend.entities.User;
 import ru.tinkoff.edu.backend.services.UserService;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
 
 @WebMvcTest(value = AuthController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @Import(ControllerTestConfiguration.class)
@@ -35,6 +37,9 @@ class AuthControllerTest {
     @MockBean
     private UserService userService;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     UserLoginDTO userLoginDTO_1 = new UserLoginDTO("123@example.com", "123");
     User user_1 = User.builder()
             .id(1L)
@@ -42,26 +47,18 @@ class AuthControllerTest {
             .password("123")
             .build();
 
-
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    UserRegDTO userRegDTO_1 = new UserRegDTO("1234@example","1234","Daniil","Kromov");
+    UserRegDTO userRegDTO_1 = new UserRegDTO("1234@example","1234","Daniil","Gromov");
 
     User user_2 = User.builder()
             .id(2L)
             .email("1234@example")
             .password("1234")
             .firstName("Daniil")
-            .lastName("Kromov")
+            .lastName("Gromov")
             .build();
 
-    String jsonUser = objectMapper.writeValueAsString(userRegDTO_1);
-
-    public AuthControllerTest() throws JsonProcessingException {
-    }
-
     @Test
-    void loginUser_thenStatus200() throws Exception {
+    void post_loginUser_thenStatus200() throws Exception {
         when(userService.readByUserLoginDTO(userLoginDTO_1)).thenReturn(user_1);
 
         mockMvc.perform(post("/api/auth/login")
@@ -73,16 +70,39 @@ class AuthControllerTest {
                 .andExpect(header().longValue("Location", user_1.getId()));
     }
 
+    @Test
+    void post_loginUserWithEntityNotFoundException_thenStatus400() throws Exception{
+        when(userService.readByUserLoginDTO(userLoginDTO_1)).thenThrow(new EntityNotFoundException("User not exist!"));
+
+        mockMvc.perform(post("/api/auth/login")
+                        .param("email", userLoginDTO_1.getEmail())
+                        .param("password", userLoginDTO_1.getPassword())
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
 
     @Test
-    void registrationUser_thenStatus201() throws Exception {
+    void post_registrationUser_thenStatus201() throws Exception {
         when (userService.create(userRegDTO_1)).thenReturn(user_2);
 
         mockMvc.perform(post("/api/auth/registration")
-                        .content(jsonUser)
+                        .content(objectMapper.writeValueAsString(userRegDTO_1))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(header().longValue("Location", user_2.getId()));
+    }
+
+    @Test
+    void post_registrationUser_withException_thenStatus201() throws Exception {
+        when (userService.create(userRegDTO_1)).thenThrow(new EntityExistsException("User already exist!"));
+
+        mockMvc.perform(post("/api/auth/registration")
+                        .content(objectMapper.writeValueAsString(userRegDTO_1))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
---
+++ backend/src/test/java/ru/tinkoff/edu/backend/controllers/profile/settings/MentorProfileSettingsControllerTest.java
@@ -0,0 +1,16 @@
+package ru.tinkoff.edu.backend.controllers.profile.settings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MentorProfileSettingsControllerTest {
+
+    @Test
+    void getMentorSettings() {
+    }
+
+    @Test
+    void editMentorSettings() {
+    }
+}
