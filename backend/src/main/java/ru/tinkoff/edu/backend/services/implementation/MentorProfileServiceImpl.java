ce74b4b1d07cde5fe8277c15b7129018ef742470
Nikita Vorobev <nik17052001@outlook.com>
2023-04-25 22:56:42 +0600

    add mappers and refactor code

--- backend/src/main/java/ru/tinkoff/edu/backend/services/implementation/MentorProfileServiceImpl.java
+++ backend/src/main/java/ru/tinkoff/edu/backend/services/implementation/MentorProfileServiceImpl.java
@@ -9,7 +9,6 @@ import ru.tinkoff.edu.backend.dto.profile.settings.UserEditMentorDTO;
 import ru.tinkoff.edu.backend.dto.profile.settings.WorkExperienceDTO;
 import ru.tinkoff.edu.backend.entities.*;
 import ru.tinkoff.edu.backend.enums.FileStorageLocation;
-import ru.tinkoff.edu.backend.exception.IncorrectDateTimeException;
 import ru.tinkoff.edu.backend.repositories.EducationRepository;
 import ru.tinkoff.edu.backend.repositories.QualificationRepository;
 import ru.tinkoff.edu.backend.repositories.UserRepository;
@@ -18,9 +17,15 @@ import ru.tinkoff.edu.backend.services.FileStorageService;
 import ru.tinkoff.edu.backend.services.MentorProfileService;
 
 import javax.persistence.EntityNotFoundException;
-import java.time.Year;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static ru.tinkoff.edu.backend.mappers.EducationMapper.educationDTOToEducation;
+import static ru.tinkoff.edu.backend.mappers.EducationMapper.educationDTOToEducations;
+import static ru.tinkoff.edu.backend.mappers.UserMapper.userToUserEditMentorDTO;
+import static ru.tinkoff.edu.backend.mappers.WorkExperienceMapper.workExperienceDTOToWorkExperiences;
+import static ru.tinkoff.edu.backend.mappers.WorkExperienceMapper.workExperienceToWorkExperienceDTOs;
 
 @Service
 public class MentorProfileServiceImpl implements MentorProfileService {
@@ -43,38 +48,37 @@ public class MentorProfileServiceImpl implements MentorProfileService {
 
     @Override
     public UserEditMentorDTO getMentorInfo(Long id) {
-        User userFromDB = userRepository.getReferenceById(id);
-        UserEditMentorDTO user = new UserEditMentorDTO();
-        user.setAboutMeAsMentor(userFromDB.getAboutAsMentor());
-        user.setIsEnabledMentorStatus(userFromDB.getIsEnabledMentorStatus());
-        user.setMentorSpecializations(userFromDB.getMentorSpecializations());
+        return userToUserEditMentorDTO(userRepository.getReferenceById(id));
+    }
 
-        Set<EducationDTO> educations = userFromDB
-                .getEducation()
-                .stream()
-                .map(e -> EducationDTO.builder()
-                        .yearStart(e.getYearStart())
-                        .yearEnd(e.getYearEnd())
-                        .qualificationId(e.getQualification().getId())
-                        .educationalInstitution(e.getEducationalInstitution())
-                        .qualificationNameWithCode(e.getQualification().getNameWithCode())
-                        .build())
-                .collect(Collectors.toSet());
+    private Qualification getQualificationById(Long id) {
+        return qualificationRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Qualification not found"));
+    }
 
-        Set<WorkExperienceDTO> workExperiences = userFromDB
-                .getWorkExperiences()
-                .stream()
-                .map(e -> WorkExperienceDTO.builder()
-                        .yearStart(e.getId().getYearStart())
-                        .yearEnd(e.getYearEnd())
-                        .placeOfWork(e.getId().getPlaceOfWork())
-                        .build())
-                .collect(Collectors.toSet());
+    private void deleteCertificateResources(Set<String> certificateResources) {
+        certificateResources.forEach(
+                e -> fileStorageService
+                        .deleteFromFileStorageLocation(FileStorageLocation.USER_CERTIFICATES, e)
+        );
+    }
 
-        user.setCertificatesResource(userFromDB.getCertificateResources());
-        user.setEducations(educations);
-        user.setWorkExperiences(workExperiences);
-        return user;
+    private Set<String> getCertificateResourcesSet(MultipartFile[] certificates, Long id) {
+        if (certificates == null) {
+            return Collections.emptySet();
+        }
+
+        Set<String> certificateResourcesSet = new HashSet<>(certificates.length * 2);
+        IntStream.range(0, certificates.length)
+                .forEach(i -> {
+                    String certificateResource = fileStorageService
+                            .save(FileStorageLocation.USER_CERTIFICATES, certificates[i], id + "_" + i);
+                    if (!certificateResource.isEmpty()) {
+                        certificateResourcesSet.add(certificateResource);
+                    }
+                });
+        return certificateResourcesSet;
     }
 
     @Transactional
@@ -85,60 +89,14 @@ public class MentorProfileServiceImpl implements MentorProfileService {
         userFromDB.setIsEnabledMentorStatus(user.getIsEnabledMentorStatus());
         userFromDB.setMentorSpecializations(user.getMentorSpecializations());
 
-        Set<Education> educations = Optional.ofNullable(user.getEducations())
-                .orElse(Collections.emptySet())
-                .stream()
-                .map(e -> {
-                            Qualification qualification = qualificationRepository
-                                    .findById(e.getQualificationId())
-                                    .orElseThrow(() -> new EntityNotFoundException("Qualification not found"));
-                            validateYearRange(e.getYearStart(), e.getYearEnd());
-                            validateYearStart(e.getYearStart());
-                            return Education.builder()
-                                    .yearStart(e.getYearStart())
-                                    .yearEnd(e.getYearEnd())
-                                    .educationalInstitution(e.getEducationalInstitution())
-                                    .id(new EducationPK(userFromDB.getId(), qualification.getId()))
-                                    .qualification(qualification)
-                                    .user(userFromDB)
-                                    .build();
-                        }
-                )
-                .collect(Collectors.toSet());
+        Set<Education> educations =
+                educationDTOToEducations(user.getEducations(), userFromDB, this::getQualificationById);
 
-        Set<WorkExperience> workExperiences = Optional.ofNullable(user.getWorkExperiences())
-                .orElse(Collections.emptySet())
-                .stream()
-                .map(e -> {
-                            validateYearRange(e.getYearStart(), e.getYearEnd());
-                            validateYearStart(e.getYearStart());
-                            return WorkExperience.builder()
-                                    .id(new WorkExperiencePK(userFromDB.getId(), e.getPlaceOfWork(), e.getYearStart()))
-                                    .user(userFromDB)
-                                    .yearEnd(e.getYearEnd())
-                                    .build();
-                        }
-                )
-                .collect(Collectors.toSet());
+        Set<WorkExperience> workExperiences =
+                workExperienceDTOToWorkExperiences(user.getWorkExperiences(), userFromDB);
 
-        userFromDB.getCertificateResources().forEach(
-                e -> fileStorageService
-                        .deleteFromFileStorageLocation(FileStorageLocation.USER_CERTIFICATES, e)
-        );
-
-        if (certificates != null) {
-            Set<String> certificateResourcesSet = new HashSet<>(certificates.length * 2);
-
-            for (int i = 0; i < certificates.length; ++i) {
-                String certificateResource = fileStorageService
-                        .save(FileStorageLocation.USER_CERTIFICATES, certificates[i], id + "_" + i);
-                if (!certificateResource.isEmpty()) {
-                    certificateResourcesSet.add(certificateResource);
-                }
-            }
-
-            userFromDB.setCertificateResources(certificateResourcesSet);
-        }
+        deleteCertificateResources(userFromDB.getCertificateResources());
+        userFromDB.setCertificateResources(getCertificateResourcesSet(certificates, id));
 
         educationRepository.deleteEducationsByUser(userFromDB);
         educationRepository.saveAll(educations);
@@ -147,29 +105,6 @@ public class MentorProfileServiceImpl implements MentorProfileService {
         workExperienceRepository.saveAll(workExperiences);
 
         userRepository.save(userFromDB);
-    }
-
-    /**
-     * Проверяет правильность диапазона лет и генерирует исключение, если год начала меньше года окончания.
-     *
-     * @param yearStart дата начала.
-     * @param yearEnd   дата конца.
-     */
-    protected void validateYearRange(Integer yearStart, Integer yearEnd) {
-        if (!Objects.isNull(yearEnd) && yearStart > yearEnd) {
-            throw new IncorrectDateTimeException("The start year cannot be less");
-        }
-    }
-
-    /**
-     * Проверяет правильность начального года и генерирует исключение, если он находится в будущем времени.
-     *
-     * @param yearStart год начала.
-     */
-    protected void validateYearStart(Integer yearStart) {
-        if (yearStart > Year.now().getValue()) {
-            throw new IncorrectDateTimeException("The beginning cannot be in the future time");
-        }
     }
 
     @Override
