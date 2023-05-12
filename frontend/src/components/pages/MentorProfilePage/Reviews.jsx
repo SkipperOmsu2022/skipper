76c630ba0485aa4cd5a90f93a1880d5d135471bb
Ilya <rogachevskii01@gmail.com>
2023-05-12 22:10:50 +0600

    reviews modal layout

--- frontend/src/components/pages/MentorProfilePage/Reviews.jsx
+++ frontend/src/components/pages/MentorProfilePage/Reviews.jsx
@@ -1,3 +1,4 @@
+import ReactPaginate from 'react-paginate';
 import { useRef, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 
@@ -7,7 +8,9 @@ import Modal from "../../Modal/Modal";
 import photo from "../../../resources/profile-photo.jpg"
 import filledStar from "../../../resources/icons/filled-star.svg"
 import emptyStar from "../../../resources/icons/empty-star.svg"
+import arrow from "../../../resources/icons/arrow.svg"
 import "../../../shared/submitButton/button.scss"
+import "../../../shared/close-icon.scss"
 
 
 const Reviews = observer(() => {
@@ -213,10 +216,82 @@ const ReviewsModal = observer(() => {
 			onModalClose={() => mentorReviewsStore.setModal(false)}
 		>
 			<div className="app-section review-modal" ref={ref}>
+                <div className="review-modal-header">
+                    <img
+                        src={arrow}
+                        alt="arrow"
+                        className="arrow-icon"
+                        onClick={() => mentorReviewsStore.setModal(false)}
+                    />
+                    <div className="review-modal-header__divider"></div>
+                    <div className="review-modal-header__text">ВСЕ ОТЗЫВЫ</div>
+                    <div className="review-modal-header__divider"></div>
+                </div>
+                <div className="review-modal-body-wrapper">
+                    <div className="review-modal-body">
+                        <Review/>
+                        <Review/>
+                        <Review/>
+                        <Review/>
+                        <Review/>
+                    </div>
+                </div>
+                <div className='pagination-wrapper'>
+                    <ReactPaginate
+                        nextLabel=">"
+                        onPageChange={() => {}}
+                        forcePage={1}
+                        pageRangeDisplayed={6}
+                        marginPagesDisplayed={0}
+                        pageCount={60}
+                        previousLabel="<"
+                        breakLabel={null}
+                        renderOnZeroPageCount={null}
 
+                        previousClassName="page-item"
+                        previousLinkClassName="page-link"
+                        nextClassName="page-item"
+                        nextLinkClassName="page-link"
+                        pageClassName="page-item"
+                        activeClassName="active-link"
+                        pageLinkClassName="page-link"
+                        containerClassName="pagination"
+                    />
+                </div>
             </div>
 		</Modal>
     )
 })
+
+const Review = () => {
+    return (
+        <>
+            <div className="review">
+                <div className="review-header">
+                    <img src={photo} alt="user" className="review-header__photo"/>
+                    <div className="review-header__info">
+                        <div className="review-header__info-user-name">
+                            Азамат Имаев
+                        </div>
+                        <div className="stars-rating">
+                            <img className="star-icon" src={filledStar} alt="" />
+                            <img className="star-icon" src={filledStar} alt="" />
+                            <img className="star-icon" src={filledStar} alt="" />
+                            <img className="star-icon" src={filledStar} alt="" />
+                            <img className="star-icon" src={emptyStar} alt="" />
+                        </div>
+                    </div>
+                    <div className="review-header__date">
+                        11 Мая 2023
+                    </div>
+                </div>
+                <div className="review-content">
+                    Более 10 лет занимаюсь налогами, откатами и прочими бухгалтерскими штучками на производстве. Готов помочь с вопросами составления отчетности и прочих бухгалтерских делишек. Также неплохо готовлю и говорю на иврите. Более 10 лет занимаюсь налогами, откатами и прочими бухгалтерскими штучками на производстве. Готов помочь с вопросами составления отчетности и прочих бухгалтерских делишек.
+                </div>
+            </div>
+            <div className="divider"></div>
+        </>
+    )
+}
 
 export default Reviews;
